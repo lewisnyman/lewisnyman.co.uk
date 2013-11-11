61cb1dc0cdbb5c6d1246c0507a9537988c00c908
Lewis Nyman <hello@lewisnyman.co.uk>
2013-11-11 17:58:41 +0000

    Cleaned up jshint errors in loader.js

--- app/js/loader.js
+++ app/js/loader.js
@@ -1,28 +1,28 @@
-Modernizr.load([
-  {
-    test: Modernizr.fontface && !isTypeKitDown(),
-    yep : "//use.typekit.net/dfl4hkz.js",
-    complete: function() {
-      try { Typekit.load(); } catch(e) {
-        sessionStorage.setItem("typekitDown", 1);
-          document.documentElement.className+=' wf-inactive';
-        }
-    }
-  },
-  {
-    test: Modernizr.touch,
-    yep : "js/fastclick.js",
-    callback: function() {
-      new FastClick(document.body);
-    }
-  },
-]);
-
 function isTypeKitDown() {
-  if (sessionStorage.getItem("typekitDown")) {
+  if (sessionStorage.getItem('typekitDown')) {
     return true;
   }
   else {
     return false;
   }
 }
+
+Modernizr.load([
+  {
+    test: Modernizr.fontface && !isTypeKitDown(),
+    yep : '//use.typekit.net/dfl4hkz.js',
+    complete: function() {
+      try { Typekit.load(); } catch(e) {
+        sessionStorage.setItem('typekitDown', 1);
+          document.documentElement.className+=' wf-inactive';
+        }
+    }
+  },
+  {
+    test: Modernizr.touch,
+    yep : 'js/fastclick.js',
+    callback: function() {
+      new FastClick(document.body);
+    }
+  },
+]);
