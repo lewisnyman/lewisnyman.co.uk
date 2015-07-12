eeed46b9c99938ad2fa631938cfa7946b80aa31a
Lewis Nyman <hello@lewisnyman.co.uk>
2015-07-12 16:20:30 +0100

    Set 1.5s timeout on typekit font loading

--- app/js/loader.js
+++ app/js/loader.js
@@ -7,15 +7,35 @@ function isTypeKitDown() {
   }
 }
 
+function setTypeInactive() {
+  document.documentElement.className+=' wf-inactive';
+}
+
 Modernizr.load([
   {
     test: Modernizr.fontface && !isTypeKitDown(),
     yep : '//use.typekit.net/dfl4hkz.js',
     complete: function() {
-      try { Typekit.load(); } catch(e) {
+      var timeoutID;
+      try {
+        Typekit.load( {
+          loading: function() {
+            timeoutID = window.setTimeout(function() {
+              setTypeInactive();
+            }, 1500);
+           },
+          inactive: function() {
+            clearTimeout(timeoutID);
+           },
+          active: function() {
+            clearTimeout(timeoutID);
+           },
+        });
+      }
+      catch(e) {
         sessionStorage.setItem('typekitDown', 1);
-          document.documentElement.className+=' wf-inactive';
-        }
+        setTypeInactive();
+      }
     }
   },
   {
