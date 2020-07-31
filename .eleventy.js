ccdd6bb77ba2ae557d4b67de5baca488d00307ea
Lewis Nyman <hello@lewisnyman.co.uk>
2020-07-31 16:25:22 +0100

    Fix menu and other stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
   eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "http://rubygems.org"
-
-gem 'jekyll'
-gem 'redcarpet'
-gem 'classifier-reborn'
-gem 'fast-stemmer'
-gem 'RedCloth'
-gem 'jekyll-compose'
-gem 'jekyll-time-to-read'
