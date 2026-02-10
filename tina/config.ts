52569aa46e24b5c63f3442891e0ef8ced7fac3a1
Lewis Nyman <hello@lewisnyman.co.uk>
2026-02-10 17:27:35 +0100

    WIP tiina integration

---
+++ tina/config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "serve",
+  },
+  media: {
+    tina: {
+      mediaRoot: "src/assets/images",
+      publicFolder: "serve",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            name: "tags",
+            type: "string",
+            list: true
+          },
+          {
+            name: "metadesc",
+            type: "string"
+          },
+          {
+            name: "date",
+            type: "datetime"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
